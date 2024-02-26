ac56f21f51d068d66a782106ac13f085f1771389
leonls1 <leonlederhossturich@gmail.com>
2024-02-26 10:31:24 -0300

    added dependencies, GymApp

--- Proyects/Gym app/TrainerView/src/main/java/module-info.java
+++ Proyects/Gym app/TrainerView/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module project.trainerview {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires jakarta.persistence;
+    requires lombok;
+    requires java.xml;
 
     opens project.trainerview to javafx.fxml;
     opens project.trainerview.controller;
