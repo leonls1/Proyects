684e039b35a68003d75741d36c1a6e936ac3ea52
leonls1 <leonlederhossturich@gmail.com>
2024-03-10 19:06:30 -0300

    10/3

--- Proyects/Using_FX/mavenproject1/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module project.mavenproject1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens project.mavenproject1 to javafx.fxml;
-    exports project.mavenproject1;
-}
--- Proyects/Using_FX/mavenproject1/src/main/java/project/mavenproject1/Main.java
+++
@@ -1,8 +0,0 @@
-package project.mavenproject1;
-
-
-public class Main {
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
