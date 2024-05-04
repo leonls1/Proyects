1500f22a5fbcada22bb9bb2b632b3c9d83d58e35
leonls1 <leonlederhossturich@gmail.com>
2024-05-04 19:47:39 -0300

    added id for all elements

--- Proyects/Using_FX/CommandManager/src/main/java/project/commandmanager/controller/MainController.java
+++ Proyects/Using_FX/CommandManager/src/main/java/project/commandmanager/controller/MainController.java
@@ -6,7 +6,16 @@ package project.commandmanager.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -15,9 +24,55 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   
+    @FXML
+    private Button btnAddItem;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private ComboBox<?> cboElements;
+
+    @FXML
+    private ComboBox<?> cboPaymentType;
+
+    @FXML
+    private ComboBox<?> cboStreet;
+
+    @FXML
+    private ComboBox<?> cboType;
+
+    @FXML
+    private TableColumn<?, ?> colElement;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colSubTotal;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private Spinner<?> spnQuantity;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TextArea txtADetails;
+
+    @FXML
+    private Text txtAmount;
+
+    @FXML
+    private TextField txtNumber;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
