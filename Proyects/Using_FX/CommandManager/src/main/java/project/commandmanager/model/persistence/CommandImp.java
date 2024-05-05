b1b8b1898abca4fb7b9dc156bcac7403eb92b5b8
leonls1 <leonlederhossturich@gmail.com>
2024-05-05 14:33:24 -0300

    added structure for daos

--- Proyects/Using_FX/CommandManager/src/main/java/project/commandmanager/model/persistence/CommandImp.java
+++ Proyects/Using_FX/CommandManager/src/main/java/project/commandmanager/model/persistence/CommandImp.java
@@ -1,6 +1,118 @@
 package project.commandmanager.model.persistence;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import java.util.List;
+import project.commandmanager.model.entities.Command;
 
-public class CommandImp {
+public class CommandImp implements CommandDAO {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("command_manager_persitence");
+    EntityManager em;
+
+    @Override
+    public void create(Command command) {
+        getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(command);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+
+        }
+    }
+
+    @Override
+    public void update(Command command) {
+        getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(command);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+
+        }
+    }
+
+    @Override
+    public void delete(Command command) {
+        getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            Command c = em.merge(command);
+            em.remove(c);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+
+        }
+    }
+
+    @Override
+    public Command getById(Long id) {
+        getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+
+            transaction.commit();
+        } catch (Exception e) {
+           if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+
+        }
+    }
+
+    @Override
+    public List<Command> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+
+    private void getEntityManager() {
+        this.em = emf.createEntityManager();
+    }
 
 }
